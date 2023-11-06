08663b46484cb6ccbced15b61f9e0e04b4f87ee6
Kasun Jayasinghe <kasun_t@epiclanka.net>
2023-11-06 23:16:52 +0530

    Design Admin Panel.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppLayoutComponent} from "./layout/app.layout.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '',
+       component: AppComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
